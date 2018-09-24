5d61a424f25adb1da1caad07b0ae3a546810a528
rummolprod999 <rummolprod999@gmail.com>
2018-09-24 12:34:23 +0300

    update

--- src/main/java/enterit/tools/download_tools.kt
+++ src/main/java/enterit/tools/download_tools.kt
@@ -73,6 +73,7 @@ fun downloadWaitWithRef(urls: String): String {
     val s = StringBuilder()
     val url = URL(urls)
     val uc = url.openConnection()
+    uc.connectTimeout = 30000
     uc.addRequestProperty("User-Agent", RandomUserAgent.randomUserAgent)
     uc.connect()
     val `is`: InputStream = uc.getInputStream()
@@ -134,6 +135,7 @@ fun downloadWaitWithRef1251(urls: String): String {
     val s = StringBuilder()
     val url = URL(urls)
     val uc = url.openConnection()
+    uc.connectTimeout = 30000
     uc.addRequestProperty("User-Agent", "Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0")
     uc.connect()
     val `is`: InputStream = uc.getInputStream()
