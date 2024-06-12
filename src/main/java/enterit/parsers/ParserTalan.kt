09d030897215a57b6a73a4adc621efe8be00b760
rummolprod999 <rummolprod999@gmail.com>
2024-06-12 07:59:22 +0300

    new

--- src/main/java/enterit/parsers/ParserTalan.kt
+++ src/main/java/enterit/parsers/ParserTalan.kt
@@ -18,7 +18,9 @@ import java.util.logging.Level
 class ParserTalan : Iparser {
     init {
         System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog")
-        java.util.logging.Logger.getLogger("org.openqa.selenium").level = Level.OFF
+        java.util.logging.Logger
+            .getLogger("org.openqa.selenium")
+            .level = Level.OFF
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver")
     }
 
@@ -29,12 +31,12 @@ class ParserTalan : Iparser {
 
     override fun parser() {
         val options = ChromeOptions()
-        options.addArguments("headless")
+        // options.addArguments("headless")
         options.addArguments("disable-gpu")
         options.addArguments("no-sandbox")
         options.addArguments("window-size=1920,1080")
         val driver = ChromeDriver(options)
-        driver.manage().window().maximize();
+        driver.manage().window().maximize()
         try {
             for (i in 1..30) {
                 val urlT = "$BaseUrl$i"
@@ -44,7 +46,6 @@ class ParserTalan : Iparser {
                     logger("Error in parserList function", e.stackTrace, e)
                 }
             }
-
         } catch (e: Exception) {
             logger("Error in parser function", e.stackTrace, e)
         } finally {
@@ -52,14 +53,23 @@ class ParserTalan : Iparser {
         }
     }
 
-    private fun parserList(urlT: String, driver: ChromeDriver) {
+    private fun parserList(
+        urlT: String,
+        driver: ChromeDriver,
+    ) {
         driver.get(urlT)
         driver.switchTo().defaultContent()
         val wait = WebDriverWait(driver, timeoutB)
         Thread.sleep(15000)
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@aria-describedby = 'grid_TenderGridViewModel_info']/tbody/tr[contains(@role, 'row') and @id][10]")))
+        wait.until(
+            ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//table[@aria-describedby = 'grid_TenderGridViewModel_info']/tbody/tr[contains(@role, 'row') and @id][10]"),
+            ),
+        )
         val tenders =
-            driver.findElements(By.xpath("//table[@aria-describedby = 'grid_TenderGridViewModel_info']/tbody/tr[contains(@role, 'row') and @id]"))
+            driver.findElements(
+                By.xpath("//table[@aria-describedby = 'grid_TenderGridViewModel_info']/tbody/tr[contains(@role, 'row') and @id]"),
+            )
         tenders.forEach {
             try {
                 parserTender(it)
@@ -70,39 +80,48 @@ class ParserTalan : Iparser {
     }
 
     private fun parserTender(el: WebElement) {
-        val purNum = el.findElementWithoutException(By.xpath(".//td[1]/a"))?.text?.trim { it <= ' ' }
-            ?: ""
-        //println(purNum)
+        val purNum =
+            el.findElementWithoutException(By.xpath(".//td[1]/a"))?.text?.trim { it <= ' ' }
+                ?: ""
+        // println(purNum)
         if (purNum == "") {
             logger("cannot find purNum in tender")
             return
         }
-        val hrefL = el.findElementWithoutException(By.xpath(".//td[1]/a"))?.getAttribute("href")?.trim({ it <= ' ' })
-            ?: ""
-        val hrefT = el.findElementWithoutException(By.xpath(".//td[2]/a"))?.getAttribute("href")?.trim({ it <= ' ' })
-            ?: ""
+        val hrefL =
+            el.findElementWithoutException(By.xpath(".//td[1]/a"))?.getAttribute("href")?.trim({ it <= ' ' })
+                ?: ""
+        val hrefT =
+            el.findElementWithoutException(By.xpath(".//td[2]/a"))?.getAttribute("href")?.trim({ it <= ' ' })
+                ?: ""
         if (hrefL == "" || hrefT == "") {
             logger("cannot find hrefs in tender", purNum)
             return
         }
-        val purName = el.findElementWithoutException(By.xpath(".//td[2]/a"))?.text?.trim({ it <= ' ' })
-            ?: ""
-        val datePubT = el.findElementWithoutException(By.xpath(".//td[7]"))?.text?.trim({ it <= ' ' })
-            ?: ""
+        val purName =
+            el.findElementWithoutException(By.xpath(".//td[2]/a"))?.text?.trim({ it <= ' ' })
+                ?: ""
+        val datePubT =
+            el.findElementWithoutException(By.xpath(".//td[7]"))?.text?.trim({ it <= ' ' })
+                ?: ""
         val pubDate = datePubT.getDateFromString(formatterGpn)
-        val endDateT = el.findElementWithoutException(By.xpath(".//td[8]"))?.text?.trim({ it <= ' ' })
-            ?: ""
+        val endDateT =
+            el.findElementWithoutException(By.xpath(".//td[8]"))?.text?.trim({ it <= ' ' })
+                ?: ""
         val endDate = endDateT.getDateFromString(formatterGpn)
         if (pubDate == Date(0L) && endDate == Date(0L)) {
             logger("cannot find dates in tender", hrefL, datePubT, endDateT)
             return
         }
-        val status = el.findElementWithoutException(By.xpath(".//td[3]"))?.text?.trim({ it <= ' ' })
-            ?: ""
-        val placingWayName = el.findElementWithoutException(By.xpath(".//td[4]"))?.text?.trim({ it <= ' ' })
-            ?: ""
-        val urlOrg = el.findElementWithoutException(By.xpath(".//td[9]/a"))?.getAttribute("href")?.trim({ it <= ' ' })
-            ?: ""
+        val status =
+            el.findElementWithoutException(By.xpath(".//td[3]"))?.text?.trim({ it <= ' ' })
+                ?: ""
+        val placingWayName =
+            el.findElementWithoutException(By.xpath(".//td[4]"))?.text?.trim({ it <= ' ' })
+                ?: ""
+        val urlOrg =
+            el.findElementWithoutException(By.xpath(".//td[9]/a"))?.getAttribute("href")?.trim({ it <= ' ' })
+                ?: ""
         val tn = TalanT(purNum, hrefT, hrefL, purName, pubDate, endDate, status, placingWayName, urlOrg)
         try {
             val t = TenderTalan(tn)
